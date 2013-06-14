20501e877a67b51ac61d4e28832275c8442f1393
Anonymous <gempro2012@hotmail.com>
2013-06-14 22:57:01 +0300

    jQuery Tree view Homework

---
+++ jQuery Tree View/scripts/tree-view.js
@@ -0,0 +1,10 @@
+﻿(function($){
+    $.fn.expandable = function() {
+        $(this).find('li>ul').parent().toggleClass('collapsed').click(function(ev) {
+            ev.stopPropagation();
+            $(this).toggleClass('expanded').find('>ul').toggle();
+        });
+
+       return $(this);
+    }  
+})(jQuery);
