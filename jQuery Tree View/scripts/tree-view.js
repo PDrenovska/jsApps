d19b5cc77711af2e907a7692ac0366bb4c4b8077
Anonymous <gempro2012@hotmail.com>
2013-06-21 11:31:59 +0300

    Add stop propagation for all anchor tags in menu

--- jQuery Tree View/scripts/tree-view.js
+++ jQuery Tree View/scripts/tree-view.js
@@ -1,5 +1,8 @@
 ﻿(function($){
     $.fn.expandable = function() {
+    	$(this).find("a").click(function(ev) {
+    		ev.stopPropagation();
+    	});
         $(this).find('li>ul').parent().toggleClass('collapsed').click(function(ev) {
             ev.stopPropagation();
             $(this).toggleClass('expanded').find('>ul').toggle();
